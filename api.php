7c9cf52e905ba916dc8c5340a03f18111f85b919
BANKA2017 <kdwnil@hotmail.com>
2021-05-15 13:27:57 +0800

    update: Use static function in class getBDUSS add: Add variable $AllowedReferrer for private used

--- api.php
+++ api.php
@@ -3,6 +3,7 @@
  * @banka2017 & KD·NETWORK
  */
 $origin = "";
+$AllowedReferrer = "";// 'a.com|b.com|c.com' multiple domains is supported
 header('content-type: application/json;charset=UTF-8');
 header('access-control-allow-origin: '. ($origin ? $origin : '*'));
 header('access-control-allow-methods: GET');
@@ -10,7 +11,7 @@ header('X-XSS-Protection: 1; mode=block');
 header('X-Frame-Options: sameorigin');
 $get = $_GET;
 $r = ["errno" => -1, "msg" => "Forbidden", "data" => []];
-if (isset($get["m"])) {
+if (($AllowedReferrer === "" || preg_match('/(^|.)(' . $AllowedReferrer . ')$/', $_SERVER["HTTP_HOST"])) && isset($get["m"])) {
     switch($get["m"]){
         case "getqrcode":
             $r["data"] = getBDUSS::getqrcode();
@@ -35,7 +36,7 @@ if (isset($get["m"])) {
 echo json_encode($r, JSON_UNESCAPED_UNICODE);
 
 class getBDUSS{
-    private function scurl (string $url = "localhost", int $timeout = 60) :string {
+    private static function scurl (string $url = "localhost", int $timeout = 60) :string {
         $ch = curl_init();
         curl_setopt($ch,CURLOPT_URL, $url);
         curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.78 Safari/537.36');
@@ -43,7 +44,7 @@ class getBDUSS{
         curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
         return curl_exec($ch);
     }
-    public function getqrcode() :array {
+    public static function getqrcode() :array {
         $resp = ["sign" => null, "imgurl" => null];
         $get_qrcode = json_decode(self::scurl("https://passport.baidu.com/v2/api/getqrcode?lp=pc"), true);
         if(isset($get_qrcode["imgurl"]) && isset($get_qrcode["sign"])){
@@ -51,7 +52,7 @@ class getBDUSS{
         }
         return $resp;
     }
-    public function get_real_bduss(string $sign, bool $full) :array{
+    public static function get_real_bduss(string $sign, bool $full) :array{
         //status code
         //errno不等于0或1时需要要求更换二维码及sign
         //-1 更换二维码
