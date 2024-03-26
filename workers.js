15dd7cd060bfb7727e13fe513acb43d778983fd5
BANKA2017 <kdwnil@hotmail.com>
2024-03-26 22:12:45 +0800

    chore: remove some unnecessary code and update user agent update: use Function() + callback to replace RegExp

--- workers.js
+++ workers.js
@@ -13,8 +13,10 @@ const init = {
 }
 
 const globalHeaders = new Headers({
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
 })
+
+const callback = "get_object_value"
 
 addEventListener('fetch', event => {
     return event.respondWith(switchRouter(event.request.url, event))
@@ -60,35 +62,38 @@ async function getqrcode() {
 
 async function getBduss(sign, full = false) {
     let resp = { status: 1, bduss: "", msg: "", fullmode: false }
-    let response = await (await fetch("https://passport.baidu.com/channel/unicast?channel_id=" + sign + "&callback=a", { headers: globalHeaders })).text()
+    let response = await (await fetch("https://passport.baidu.com/channel/unicast?channel_id=" + sign + "&callback=" + callback, { headers: globalHeaders })).text()
     if (response) {
-        const errno = parseInt(/"errno":([\-0-9]+)(?:,|})/.exec(response)[1])
+        const parsedData = callbackfunc(response)
+
+        const errno = parsedData.errno
         if (errno === 1) {
             resp.status = 1
         } else if (errno === 0) {
-            const channel_v = JSON.parse(/"channel_v":"(.*)"}\)/.exec(response)[1].replace(/\\/gm, ''))
+            const channel_v = JSON.parse(parsedData.channel_v)
             if (channel_v.status) {
                 resp.status = 0
                 resp.msg = "Continue"
             } else {
-                const userData = await JSON.parse(((await (await fetch('https://passport.baidu.com/v3/login/main/qrbdusslogin?bduss=' + channel_v.v, { headers: globalHeaders })).text()).replace(/'([^'']+)'/gm, `"$1"`)).replace(/\\&/gm, "&"))
+                const userData = callbackfunc(await (await fetch('https://passport.baidu.com/v3/login/main/qrbdusslogin?callback=' + callback + '&bduss=' + channel_v.v, { headers: globalHeaders })).text())
+
                 if (userData && userData.code === "110000") {
                     resp.status = 2
                     resp.msg = "Success"
-                    resp.bduss = await userData.data.session.bduss
+                    resp.bduss = userData.data.session.bduss
                     if (full) {
                         resp.fullmode = true
-                        let fullModeData = {}
-                        fullModeData.ptoken = await userData.data.session.ptoken
-                        fullModeData.stoken = await userData.data.session.stoken
-                        fullModeData.ubi = await userData.data.session.ubi
-                        fullModeData.hao123Param = await userData.data.hao123Param
-                        fullModeData.username = await userData.data.user.username
-                        fullModeData.userId = await userData.data.user.userId
-                        fullModeData.portraitSign = await userData.data.user.portraitSign
-                        fullModeData.displayName = await userData.data.user.displayName
-                        fullModeData.stokenList = parseStoken(await userData.data.session.stokenList)
-                        resp.data = fullModeData
+                        resp.data = {
+                            ptoken: userData.data.session.ptoken,
+                            stoken: userData.data.session.stoken,
+                            ubi: userData.data.session.ubi,
+                            hao123Param: userData.data.hao123Param,
+                            username: userData.data.user.username,
+                            userId: userData.data.user.userId,
+                            portraitSign: userData.data.user.portraitSign,
+                            displayName: userData.data.user.displayName,
+                            stokenList: parseStoken(userData.data.session.stokenList)
+                        }
                     }
                 }
             }
@@ -103,10 +108,9 @@ async function getBduss(sign, full = false) {
 }
 
 function parseStoken(stokenList) {
-    let tmpStokenList = {}
-    JSON.parse(stokenList.replace(/&quot;/gm, '"')).map(x => {
-        let tmpX = x.split('#')
-        tmpStokenList[tmpX[0]] = tmpX[1]
-    })
-    return tmpStokenList
+    return Object.fromEntries(JSON.parse(stokenList.replace(/&quot;/gm, '"')).map(x => x.split('#')))
+}
+
+function callbackfunc(callback_str) {
+    return Function(`const ${callback} = (obj) => obj; return ${callback_str}`)()
 }
