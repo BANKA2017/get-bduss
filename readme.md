acd5cbd1d01a3dbd76c897c836e8f2c30d0ee303
BANKA2017 <kdwnil@hotmail.com>
2024-04-21 05:28:32 +0800

    rollback: Code generation from strings disallowed for this context update: REFERER and ROUTER to control requests update: support hash callback

--- readme.md
+++ readme.md
@@ -1,21 +1,27 @@
 # BDUSS获取
 
-## workers
+## workers 后端
 
 在 <https://workers.cloudflare.com> 新建一个workers，将 `workers.js` 的内容复制粘贴就完成了
 
-- 可以设置环境变量或者取消注释设置 `ORIGIN`
-- 可以设置环境变量或者取消注释设置 `AllowedReferrer`
+### 环境变量
 
-## php
+| 变量名    | 默认值   | 格式                                                                   | 备注                                                                                       |
+| :-------- | :------- | :--------------------------------------------------------------------- | :----------------------------------------------------------------------------------------- |
+| `REFERER` | `*`      | `http://example.com/\|https://example.com/`                            | 不需要路径                                                                                 |
+| `ROUTER`  | 空字符串 | `http://example.com/\|https://example.com/path/\|https://example.com/` | 即触发路由的部分（建议将 设置 -> 触发器 -> 路由 列表里面的都放进去，注意要去掉正则表达式） |
+
+## ~~php 后端~~
 
 - 可以设置 `$origin`
+
+精力有限，不再维护 PHP 版后端，源码锁定
 
 ## 示例
 
 `/index.html` 是演示站 <https://bduss.nest.moe> 的源码，要直接使用需要修改或删除下述几项
 
-- api 地址，搜索 `!!! DEPLOY YOUR OWN API ENDPOINT !!!` 找到夹在中间的变量修改值即可，演示站后端随时会失效，请尽量自行部署
+- api 地址，搜索 `!!! DEPLOY YOUR OWN API ENDPOINT !!!` 找到夹在中间的变量修改值即可，演示站后端已开访问校验，请尽量自行部署后端
 
 ## 环境要求
 
@@ -29,16 +35,26 @@ php-curl
 ## 关于回调与stoken
 
 - 请将回调链接进行`base64`/`base64url`编码后以`hash`形式添加到连接到网站的链接中
-- 默认不提供`stoken`，如果需要请在`query`的`stoken_type`中体现，至于这个`stoken_type`的可用值请自行寻找
+- 默认不提供`stoken`，如果需要请在`query`/`hash`的`stoken_type`中体现，至于这个`stoken_type`的可用值请自行寻找
 
 参考格式
 
 ```javascript
-//https://bduss.nest.moe/#/aHR0cHM6Ly9leGFtcGxlLmNvbS8/c3Rva2VuX3R5cGU9dGI=
-//https://bduss.nest.moe/#/aHR0cHM6Ly9leGFtcGxlLmNvbS8_c3Rva2VuX3R5cGU9dGI
-"https://bduss.nest.moe/#/" + btoa("https://example.com/?stoken_type=tb")//这样会回调会带贴吧的stoken
-//---
-"https://bduss.nest.moe/#/" + btoa("https://example.com/")//这样会不带stoken
+// query 回调带 stoken
+// https://bduss.nest.moe/#/aHR0cHM6Ly9leGFtcGxlLmNvbS8/c3Rva2VuX3R5cGU9dGI=
+// https://bduss.nest.moe/#/aHR0cHM6Ly9leGFtcGxlLmNvbS8_c3Rva2VuX3R5cGU9dGI
+"https://bduss.nest.moe/#/" + btoa("https://example.com/?stoken_type=tb")/
+// https://example.com/?stoken_type=tb&bduss=...&stoken=...
+
+// hash 回调带 stoken
+// https://bduss.nest.moe/#/aHR0cHM6Ly9leGFtcGxlLmNvbS8jL3N0b2tlbl90eXBlPXRi
+"https://bduss.nest.moe/#/" + btoa("https://example.com/#/stoken_type=tb")
+// https://example.com/#/stoken_type=tb&bduss=...&stoken=...
+
+// query 回调不带 stoken
+// https://bduss.nest.moe/#/aHR0cHM6Ly9leGFtcGxlLmNvbS8
+"https://bduss.nest.moe/#/" + btoa("https://example.com/")
+// https://example.com/?stoken_type=tb&bduss=...
 ```
 
 
--- workers.js
+++ workers.js
@@ -1,37 +1,52 @@
 'use strict'
-//const ORIGIN = ''//Environment Variables
-const AllowedReferrer = ''// 'a.com|b.com|c.com' multiple domains is supported
-const init = {
+const responseInit = (referrer = '*') => ({
     status: 403,
     headers: {
         'content-type': 'application/json;charset=UTF-8',
-        'access-control-allow-origin': (typeof ORIGIN !== 'undefined') ? ORIGIN : '*',
+        'access-control-allow-origin': referrer,
         'access-control-allow-methods': 'GET',
         'X-XSS-Protection': '1; mode=block',
         'X-Frame-Options': 'sameorigin',
     },
-}
-
-const globalHeaders = new Headers({
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
 })
 
-const callback = "get_object_value"
-
-addEventListener('fetch', event => {
-    return event.respondWith(switchRouter(event.request.url, event))
+const requestHeaders = new Headers({
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
 })
 
-async function switchRouter(url) {
-    const parseUrl = new URL(url)
+// const callback = ''//"get_object_value_" + Date.now()
+
+async function switchRouter(request, env) {
+    const parseUrl = new URL(request.url)
     const _searchParams = parseUrl.searchParams
     let resp = {
         "errno": -1,
         "msg": "Forbidden",
         "data": []
     }
-    if (AllowedReferrer === '' || RegExp('(^|\.)(' + AllowedReferrer + ')$').test(parseUrl.host)) {
-        init.status = 200
+
+    // referrer
+    let referrer = env.REFERER ? env.REFERER : '*'
+    if (referrer !== '*') {
+        referrer = referrer.split('|').find(x => x.trim() === request.headers.get('referer'))
+    }
+
+    // router
+    let router = env.ROUTER ? env.ROUTER : ''
+    if (router !== '') {
+        router = router.split('|').some(x => {
+            try {
+                const parsedRouter = new URL(x.trim())
+                return parseUrl.host === parsedRouter.host && parseUrl.pathname === parsedRouter.pathname
+            } catch {
+                return false
+            }
+        })
+    }
+
+    const respInit = responseInit('*')
+    if ((referrer === '*' || referrer !== undefined) && router) {
+        respInit.status = 200
         switch (_searchParams.get("m")) {
             case "getqrcode":
                 resp.data = await getqrcode()
@@ -52,30 +67,28 @@ async function switchRouter(url) {
         }
     }
 
-    return new Response(JSON.stringify(resp), init)
+    return new Response(JSON.stringify(resp), respInit)
 }
 
 async function getqrcode() {
-    const response = await (await fetch("https://passport.baidu.com/v2/api/getqrcode?lp=pc", { headers: globalHeaders })).json()
+    const response = await (await fetch("https://passport.baidu.com/v2/api/getqrcode?lp=pc", { headers: requestHeaders })).json()
     return { sign: response.sign, imgurl: response.imgurl }
 }
 
 async function getBduss(sign, full = false) {
     let resp = { status: 1, bduss: "", msg: "", fullmode: false }
-    let response = await (await fetch("https://passport.baidu.com/channel/unicast?channel_id=" + sign + "&callback=" + callback, { headers: globalHeaders })).text()
+    let response = await (await fetch("https://passport.baidu.com/channel/unicast?channel_id=" + sign + "&callback=a", { headers: requestHeaders })).text()
     if (response) {
-        const parsedData = callbackfunc(response)
-
-        const errno = parsedData.errno
+        const errno = parseInt(/"errno":([\-0-9]+)(?:,|})/.exec(response)[1])
         if (errno === 1) {
             resp.status = 1
         } else if (errno === 0) {
-            const channel_v = JSON.parse(parsedData.channel_v)
+            const channel_v = JSON.parse(/"channel_v":"(.*)"}\)/.exec(response)[1].replace(/\\/gm, ''))
             if (channel_v.status) {
                 resp.status = 0
                 resp.msg = "Continue"
             } else {
-                const userData = callbackfunc(await (await fetch('https://passport.baidu.com/v3/login/main/qrbdusslogin?callback=' + callback + '&bduss=' + channel_v.v, { headers: globalHeaders })).text())
+                const userData = await JSON.parse(((await (await fetch('https://passport.baidu.com/v3/login/main/qrbdusslogin?bduss=' + channel_v.v, { headers: requestHeaders })).text()).replace(/'([^'']+)'/gm, `"$1"`)).replace(/\\&/gm, "&"))
 
                 if (userData && userData.code === "110000") {
                     resp.status = 2
@@ -111,6 +124,10 @@ function parseStoken(stokenList) {
     return Object.fromEntries(JSON.parse(stokenList.replace(/&quot;/gm, '"')).map(x => x.split('#')))
 }
 
-function callbackfunc(callback_str) {
-    return Function(`const ${callback} = (obj) => obj; return ${callback_str}`)()
-}
+// function callbackfunc(callback_str) {
+//     return Function(`const ${callback} = (obj) => obj; return ${callback_str}`)()
+// }
+
+addEventListener('fetch', event => {
+    return event.respondWith(switchRouter(event.request, globalThis))
+})
